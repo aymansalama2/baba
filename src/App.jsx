0c09ed649c23b88c7fbc58181bec265e8bee381b
aymansalama2 <113627548+aymansalama2@users.noreply.github.com>
2024-01-24 14:03:02 +0100

    1234567

--- src/App.jsx
+++ src/App.jsx
@@ -30,7 +30,7 @@ function App() {
         <video ref={videoRef} className="w-100 max-width-800 mb-4" controls>
           <source src="2.mp4" type="video/mp4" />
         </video>
-        <video ref={videoRef} className="w-100 max-width-800 mb-4" controls>
+        <video ref={videoRef} className="w-100 max-width-800 mb-4 bg-dark" controls>
           <source src="3.mp4" type="video/mp4" />
         </video>
         <p className="text-success">Répétitions restantes : {72 - repetitionCount}</p>
